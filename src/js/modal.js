1fcb3fe67bae8fb33f75709b7bb8ff6219a45111
Sofi Dobriak <sofi.dobriak@gmail.com>
2025-03-18 11:51:47 +0200

    add esc bth logic

--- src/js/modal.js
+++ src/js/modal.js
@@ -9,15 +9,25 @@ export function openModal() {
   refs.closeButton.addEventListener('click', closeModal);
   refs.modalBackdrop.addEventListener('click', closeModal);
 
+  document.addEventListener('keydown', onEscapeClick);
+
   updateWishList();
   updateCardList();
 }
 
 export function closeModal(e) {
-  if (e && (e.target === refs.modalBackdrop || e.target === refs.closeButton)) {
+  if (!e || e.target === refs.modalBackdrop || e.target === refs.closeButton) {
     refs.modalBackdrop.classList.remove('modal--is-open');
 
     refs.closeButton.removeEventListener('click', closeModal);
     refs.modalBackdrop.removeEventListener('click', closeModal);
+
+    document.removeEventListener('keydown', onEscapeClick);
+  }
+}
+
+function onEscapeClick(e) {
+  if (e.key === 'Escape') {
+    closeModal();
   }
 }
